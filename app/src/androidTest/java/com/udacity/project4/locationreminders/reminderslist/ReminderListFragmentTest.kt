9b6c7dc765e83d17801563f04395379f87658f35
Mohaned <memo.yossry@gmail.com>
2022-10-07 01:17:44 +0200

    feat: Added test for fragment navigation

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -6,14 +6,18 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -35,7 +39,26 @@ class ReminderListFragmentTest {
     fun  setupFragment(){
 
     }
-//    TODO: test the navigation of the fragments.
+    @Test
+    fun clickFAB_navigateToSaveReminderFragment() = runTest {
+        val scenario = launchFragmentInContainer<ReminderListFragment>(
+            Bundle(),
+            R.style.AppTheme
+        )
+        val navController = mock(NavController::class.java)
+
+        scenario.onFragment{
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        onView(withId(R.id.addReminderFAB))
+            .perform(click())
+
+
+        verify(navController).navigate(
+            ReminderListFragmentDirections.toSaveReminder()
+        )
+    }
 //    TODO: test the displayed data on the UI.
 //    TODO: add testing for the error messages.
 }
